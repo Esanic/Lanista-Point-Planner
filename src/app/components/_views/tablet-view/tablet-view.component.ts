fb27a52a7b9dd7129edac173ad2bf36d524b8b2a
Christopher Reineborn <Christopher@zylion.se>
2024-05-05 22:10:28 +0200

    Added tablet view and fixed some buttons alignment

--- src/app/components/_views/tablet-view/tablet-view.component.ts
+++ src/app/components/_views/tablet-view/tablet-view.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { TableComponent } from '../../_tables/table/table.component';
+import { MobileNavComponent } from '../../_nav/mobile-nav/mobile-nav.component';
 
 @Component({
   selector: 'app-tablet-view',
   standalone: true,
-  imports: [],
+  imports: [TableComponent, MobileNavComponent],
   templateUrl: './tablet-view.component.html',
-  styleUrl: './tablet-view.component.css'
+  styleUrl: './tablet-view.component.css',
 })
-export class TabletViewComponent {
-
-}
+export class TabletViewComponent {}
