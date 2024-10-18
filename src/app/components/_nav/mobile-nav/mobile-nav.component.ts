c2ea0fe7b0f44a8d86166d0f054b8ac7c415621e
Christopher Reineborn <Christopher@zylion.se>
2024-10-18 22:22:32 +0200

    Fixed width of canvas for mobile view

--- src/app/components/_nav/mobile-nav/mobile-nav.component.ts
+++ src/app/components/_nav/mobile-nav/mobile-nav.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { ArmoryComponent } from '../../_containers/armory/armory.component';
 import { SettingsComponent } from '../../_containers/settings/settings.component';
 import { BuildsComponent } from '../../_containers/builds/builds/builds.component';
+import { DonateComponent } from '../../_containers/donate/donate.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-mobile-nav',
   standalone: true,
-  imports: [ArmoryComponent, SettingsComponent, BuildsComponent],
+  imports: [ArmoryComponent, SettingsComponent, BuildsComponent, DonateComponent, CommonModule],
   templateUrl: './mobile-nav.component.html',
   styleUrl: './mobile-nav.component.css',
 })
-export class MobileNavComponent {}
+export class MobileNavComponent {
+  public screenWidth: number = 0;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    this.screenWidth = event.target.innerWidth;
+  }
+
+  public widthOfCanvas(): string {
+    return this.screenWidth < 768 ? 'w-100' : 'w-50';
+  }
+}
